7db87b353be767ba4f5502538c2c2733ef35888e
Jonathan Heredia <shakatoti1618@gmail.com>
2026-07-28 16:59:49 -0500

    feat: conectar CRUD de tareas al Dashboard

--- src/components/tasks/TaskItem.tsx
+++ src/components/tasks/TaskItem.tsx
@@ -1,4 +1,4 @@
-import type { Task } from "../../../types/Task";
+import type { Task } from "../../types/Task";
 
 interface Props {
   task: Task;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,15 @@
 import { logout } from "../services/auth";
 import { useNavigate } from "react-router-dom";
+import { useTasks } from "../hooks/useTasks";
+import TaskForm from "../components/tasks/TaskForm";
+import TaskList from "../components/tasks/TaskList";
 
 function Dashboard() {
   const navigate = useNavigate();
+  const { tasks, loading, addTask, removeTask, completeTask } = useTasks();
 
   async function handleLogout() {
     await logout();
-
     navigate("/");
   }
 
@@ -14,9 +17,15 @@ function Dashboard() {
     <main>
       <h1>Dashboard</h1>
 
-      <button onClick={handleLogout}>
-        Cerrar sesión
-      </button>
+      <button onClick={handleLogout}>Cerrar sesión</button>
+
+      <TaskForm onAdd={addTask} />
+
+      {loading ? (
+        <p>Cargando tareas...</p>
+      ) : (
+        <TaskList tasks={tasks} onDelete={removeTask} onToggle={completeTask} />
+      )}
     </main>
   );
 }
